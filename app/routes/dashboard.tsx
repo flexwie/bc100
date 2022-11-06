ba2dc806123b3d8098cbf0955c3f287d0333be0f
Felix Wieland <mail@felixwie.com>
2022-11-06 21:14:15 +0100

    changed error handling

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -179,8 +179,9 @@ export const action: ActionFunction = async (args) => {
         const body = await unstable_parseMultipartFormData(request, uploadFile);
         await addJourney(body, user!.id);
       } catch (e) {
-        throw e;
-        return json({ error: (e as Error).message });
+        return json({
+          error: (e as Error).message + "\n" + (e as Error).stack,
+        });
       }
       return json({ error: null });
     },
@@ -193,7 +194,6 @@ export const action: ActionFunction = async (args) => {
         await deleteJourney(parseInt(id!), user!.id);
         return json({ error: null });
       } catch (e) {
-        throw e;
         return json({
           error: (e as Error).message + "\n" + (e as Error).stack,
         });
