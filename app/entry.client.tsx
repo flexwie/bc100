bb29fa1659246a20f8e449c9351436f3b2120c7c
Felix Wieland <mail@felixwie.com>
2022-11-06 20:56:13 +0100

    added dsn entry

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,7 +4,7 @@ import * as Sentry from "@sentry/remix";
 import { useEffect } from "react";
 
 Sentry.init({
-  dsn: process.env["SENTRY_DSN"],
+  dsn: "https://d346f97076e14b3a9fc0f026292e0f99:53409bbea62a4d8f8baa92f5c1df8f67@o4504113167925248.ingest.sentry.io/4504113168842752",
   tracesSampleRate: 1,
   integrations: [
     new Sentry.BrowserTracing({
